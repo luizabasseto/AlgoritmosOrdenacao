717656162df8fd205d6c5fcbe162b79ac3f4cb87
Luiza Batista Basseto <luizabasseto.1@gmail.com>
2025-05-19 15:03:46 -0300

    mexendo na main

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,22 @@
 from Algoritmos import algoritmosOrdenacao
-from ManipulacaoArquivos import arquivos
+from ManipulacaoArquivos import Arquivo
 
 def main():
+
+    arquivo1 = Arquivo.__init__("Arquivo1", 1000)
+    arquivo1.createArquivoBin()
+
+    lista1 = arquivo1.ReadArquivoBin()
+    lista = Algoritmos.__init__(lista1,1000)
+
+    lista.BubbleSortPuro()
+
+
+    
+
+
+
+
     
     
-    
+main()    
--- ManipulacaoArquivos/Arquivo.py
+++ ManipulacaoArquivos/Arquivo.py
@@ -7,15 +7,17 @@ class Arquivo:
     
 
     def createArquivoBin(self):
-        nameArquivo = "ArquivoTamanho"+self.__size
-        with open (nameArquivo,'wb') as arquivo:
+        self.__name = self.__name + str(self.__size)
+        with open (self.__name,'wb') as arquivo:
             for i in range(self.__size):
                 arquivo.write(gerador.GerarNumeros())
-        return True
                 
     def ReadArquivoBin(self):
-        lista=[]
-        with open (self.__name,'rb') as arquivo:
+        lista = []
+        with open(self.__name, 'rb') as arquivo:
             for i in range(self.__size):
-                lista.append(arquivo.read())
+                bytes_lidos = arquivo.read(4)
+                if bytes_lidos:
+                    num = int.from_bytes(bytes_lidos, byteorder='little')
+                    lista.append(num)
         return lista
