a32f2f49564877839d896c16c9938fe3096899fa
luizabbasseto <luizabasseto.1@gmail.com>
2025-05-22 13:02:23 -0300

    começando main

--- Codigos/main.cpp
+++ Codigos/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+#include "algorithms.hpp"
+#include "manipArchives.hpp"
+#include "searches.hpp"
 
 int main(){
     
+    int n1 = 100;
+    int n2 = 1000;
+    int n3 = 1000000;
+    createArchiveBin("ArquivoN1",n1);
+    createArchiveBin("ArquivoN2",n2);
+    createArchiveBin("ArquivoN3",n3);
 
 
 }
